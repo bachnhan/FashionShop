43bcba5ed2c3a94330bf2f1a4ce5ebd6becff69b
Tran Kim Thanh <kimthanh932000@gmail.com>
2018-07-17 22:44:27 +0700

    commit

--- Admin/Controllers/AccountController.cs
+++ Admin/Controllers/AccountController.cs
@@ -3,16 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HmsService.Models;
+using HmsService.ViewModels;
+using HmsService.Sdk;
+using HmsService.Models.Entities;
 
 namespace Admin.Controllers
 {
     public class AccountController : Controller
     {
         // GET: Account
-        public ActionResult Login()
+        public ActionResult Login(string username, string password)
         {
-            
-            return View();
+            //AccountViewModel a = new AccountViewModel() {
+            //    Username = username,
+            //    Password = password            
+            //};
+            //AccountApi api = new AccountApi();
+            //api.Create(a);
+            SWDEntities db = new SWDEntities();
+            var user = db.Accounts.Where(q => q.Username == username && q.Password == password);
+            if(user != null)
+            {
+                return Redirect("/Home/Index");
+            }
+            return Content("Login failed");
         }
     }
 }
