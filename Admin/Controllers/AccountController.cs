c6ea83b2ff699b2b0b033743fb4d518ecf23e8af
Tran Kim Thanh <kimthanh932000@gmail.com>
2018-07-19 19:23:24 +0700

    commit

--- Admin/Controllers/AccountController.cs
+++ Admin/Controllers/AccountController.cs
@@ -17,7 +17,7 @@ namespace Admin.Controllers
             return View();
         }
         // GET: Account/Login
-        public ActionResult Login(string username, string password)
+        public ActionResult Login(string username, string password, string url)
         {
             //AccountViewModel a = new AccountViewModel() {
             //    Username = username,
@@ -25,11 +25,15 @@ namespace Admin.Controllers
             //};
             //AccountApi api = new AccountApi();
             //api.Create(a);
+        
+            url = "google.com";
             AccountApi accountapi = new AccountApi();
             var result = accountapi.Get().Where(q => q.Username == username && q.Password == password);
             if(result != null)
             {
-                return Redirect("/Home/Index");
+                //return Redirect(url);
+                //return View();
+                return Content("Login failed");
             }
             return Content("Login failed");
         }
