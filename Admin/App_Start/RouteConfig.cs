259bd51bf6fddcbe3427dd5b75de8fc6f0b1ba14
Tran Kim Thanh <kimthanh932000@gmail.com>
2018-07-21 11:42:54 +0700

    Update Product datatable

--- Admin/App_Start/RouteConfig.cs
+++ Admin/App_Start/RouteConfig.cs
@@ -14,16 +14,16 @@ namespace Admin
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
 
-            //routes.MapRoute(
-            //    name: "Default",
-            //    url: "{controller}/{action}/{id}",
-            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            //);
             routes.MapRoute(
-            name: "Default",
-            url: "{controller}/{action}/{id}",
-            defaults: new { controller = "Account", action = "Index", id = UrlParameter.Optional }
-);
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Product", action = "Index", id = UrlParameter.Optional }
+            );
+            //            routes.MapRoute(
+            //            name: "Default",
+            //            url: "{controller}/{action}/{id}",
+            //            defaults: new { controller = "Account", action = "Index", id = UrlParameter.Optional }
+            //);
         }
     }
 }
--- Admin/Controllers/ProductController.cs
+++ Admin/Controllers/ProductController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HmsService.Models;
+using HmsService.ViewModels;
+using HmsService.Sdk;
+using HmsService.Models.Entities;
 
 namespace Admin.Controllers
 {
@@ -12,6 +16,13 @@ namespace Admin.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult GetAllProducts()
+        {
+            ProductApi productApi = new ProductApi();
+            List<ProductViewModel> result = productApi.GetActive().Where(q => q.ParentProductId != null).ToList();
+            return Json(new { data = result }, JsonRequestBehavior.AllowGet);
         }
     }
 }
